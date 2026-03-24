aac465117c4efb3a71be0b7b7fa4f23b274a8832
Brian kimanzi <migelbrian3@gmail.com>
2026-03-24 03:14:16 +0300

    new code update

--- utils/apply.py
+++ utils/apply.py
@@ -1,1 +1,11 @@
-# this is a comment
+"""
+apply.py - Job Details & Application page
+This file contains ONE Function: show_apply_page()
+It is Imported and called from app.py inside the main() router.
+
+This page has TWO stages that render one at a time:
+
+    STAGE 1 - Job Detail View
+        Shows the full job description, requirements, salary, location.
+        Has an "Apply Now" button that moves to Stage 2
+"""
